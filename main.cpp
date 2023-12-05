759157a4c292efa203b93fbed8b86a7185da0ee8
GarrisonRegala <152208051+GarrisonRegala@users.noreply.github.com>
2023-12-05 10:13:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,54 @@
+#include "PlaylistNode.h"
+
+PlaylistNode::PlaylistNode() {
+    uniqueID = "none";
+    songName = "none";
+    artistName = "none";
+    songLength = 0;
+    nextNodePtr = nullptr;
+}
+
+PlaylistNode::PlaylistNode(string id, string songname, string artist, int length) {
+    uniqueID = id;
+    songName = songname;
+    artistName = artist;
+    songLength = length;
+    nextNodePtr = nullptr;
+}
+
+string PlaylistNode::GetID() const {
+    return uniqueID;
+}
+
+string PlaylistNode::GetSongName() const {
+    return songName;
+}
+
+string PlaylistNode::GetArtistName() const {
+    return artistName;
+}
+
+int PlaylistNode::GetSongLength() const {
+    return songLength;
+}
+
+PlaylistNode* PlaylistNode::GetNext() const {
+    return nextNodePtr;
+}
+
+void PlaylistNode::InsertAfter(PlaylistNode* nodePtr) {
+    this->SetNext(nodePtr->GetNext());
+    nodePtr->SetNext(this);
+}
+
+void PlaylistNode::SetNext(PlaylistNode* nodePtr) {
+    nextNodePtr = nodePtr;
+}
+
+void PlaylistNode::PrintPlaylistNode() const {
+    cout << "Unique ID: " << uniqueID << endl;
+    cout << "Song Name: " << songName << endl;
+    cout << "Artist Name: " << artistName << endl;
+    cout << "Song Length (in seconds): " << songLength << endl;
+}
 
